77ae2ff5bbf66a66d4d2f655f326d12a4ba9dedf
Uros Zoretic <uz1734@student.uni-lj.si>
2020-04-18 12:14:12 +0200

    sequence media update

--- Models/Env.go
+++ Models/Env.go
@@ -13,8 +13,6 @@ type Env struct {
 	Port string
 	MediaMetadataGrpcServer string
 	MediaMetadataGrpcPort string
-	MediaChunkMetadataServer string
-	MediaChunkMetadataPort string
 	SequenceServiceServer string
 	SequenceServicePort string
 }
@@ -26,8 +24,6 @@ func InitEnv()  {
 		Port: 						os.Getenv("PORT"),
 		MediaMetadataGrpcServer:	os.Getenv("MEDIA_METADATA_GRPC_SERVER"),
 		MediaMetadataGrpcPort:		os.Getenv("MEDIA_METADATA_GRPC_PORT"),
-		MediaChunkMetadataServer:	os.Getenv("MEDIA_CHUNKS_METADATA_GRPC_SERVER"),
-		MediaChunkMetadataPort:		os.Getenv("MEDIA_CHUNKS_METADATA_GRPC_PORT"),
 		SequenceServiceServer:		os.Getenv("SEQUENCE_SERVICE_GRPC_SERVER"),
 		SequenceServicePort:		os.Getenv("SEQUENCE_SERVICE_GRPC_PORT"),
 	}
--- router/sequenceManagerRouter.go
+++ router/sequenceManagerRouter.go
@@ -14,11 +14,8 @@ type SequenceManagerRouter struct {
 
 func (sequenceManagerRouter *SequenceManagerRouter) RegisterHandlers()  {
 	sequenceMediaController := controllers.SequenceMediaController{SequenceMediaService: services.InitSequenceMediaService()}
-	seqeunceTimeshiftController := controllers.SequenceTimeShiftController{SequenceTimeShiftService:services.InitSequenceTimeShiftService()}
 
 	(*sequenceManagerRouter).Router.HandleFunc("/sequence/{sequenceId}/media", sequenceMediaController.GetSequenceMedia).Methods("GET")
 	(*sequenceManagerRouter).Router.HandleFunc("/sequence/{sequenceId}/media/{mediaId}", sequenceMediaController.AddMediaToSequence).Methods("POST")
 	(*sequenceManagerRouter).Router.HandleFunc("/sequence/{sequenceId}/media/{mediaId}", sequenceMediaController.DeleteMediaFromSequence).Methods("DELETE")
-
-	(*sequenceManagerRouter).Router.HandleFunc("/sequence/{sequenceId}/chunks", seqeunceTimeshiftController.GetSequenceChunks).Methods("GET")
 }
